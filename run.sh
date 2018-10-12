90171e4be0341fb3364ccd8348af93fc7fc72665
Ben Johnson <bkj.322@gmail.com>
2018-10-11 21:09:10 -0400

    cleaning up

--- run.sh
+++ run.sh
@@ -14,13 +14,17 @@ make
 # FFA c++ version
 ./ffa > results/ffa
 
-python validate.py results/gtf results/ffa
-
 # python version
 python reference/sfl.py \
     --node-path _data/nodes.txt \
     --edge-path _data/edges.txt > results/reference
 
-# check results
+echo "==========================="
+echo "validate gtf ffa"
+python validate.py results/gtf results/ffa
+
+echo "validate gtf reference"
 python validate.py results/gtf results/reference
+
+echo "validate ffa reference"
 python validate.py results/ffa results/reference
